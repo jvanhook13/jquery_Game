b74cedd30568e5adaeb60b6fd0164ca52a46e884
jvanhook13 <jaredvanhook1313@gmail.com>
2020-02-17 20:29:15 -0800

    PLAYER SCORE NOW ADDS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   
 
 //declare variables/objects
-const playerScore = [] ;
+var playerScore = 0 ;
 var wins = 0 ;
-var loses = 0 ;
+var losses = 0 ;
 var randomScore = Math.floor(Math.random(19 , 121) * 121 );
     console.log(Math.floor(Math.random(19 , 121) * 121 ))
 
@@ -18,31 +18,28 @@ var randomScore = Math.floor(Math.random(19 , 121) * 121 );
 
 function clicked() {
     //push buttton value to player score array
-    console.log('playersScore' + $(this).val());
-        playerScore.push($(this).val());
+    console.log('playersScore ' + $(this).val());
+        playerScore = Number(playerScore) + Number($(this).val()) ;
         console.log('Current Score: ' + playerScore);
-       var playerScoreString = playerScore ; 
-            console.log(" Stringed array " , playerScoreString)
-        // playerScoreString.parseInt()
-       const add = (a , b) =>
-        a + b
+        if (playerScore === randomScore && playerScore !== 0) {
 
-        const sum = playerScore.reduce(add, 0)
-
+            wins++ ;
         
-
-        console.log("Summed score " + sum)
-
-    // get the sum of the array
-    // for (i=0 ; i < playerScoreString.length ; i++) {
-
-    //     if (i=0) { continue ;} ;
-    //     playerScore += playerScoreString[i] << 0;
-    //     console.log("playerscore" , playerScore)
+        } else if (playerScore > randomScore) {
+        
+            losses++ ;
+        
+        }
 
     }
 
-})
-
 //functions to count wins/losses
 
+
+
+
+
+// winLoss() ;
+console.log("wins" , wins)
+console.log("losses" , losses);
+})
