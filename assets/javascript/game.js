68056819388bde2f4ff64b12470e7281d91a12ec
jvanhook13 <jaredvanhook1313@gmail.com>
2020-02-17 22:44:32 -0800

    MMMM THAT SWEET SWEET CSS GRID. Everything displays but wins/losses still doesnt increment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var playerScore = 0 ;
 var wins = 0 ;
 var losses = 0 ;
 var randomScore = Math.floor(Math.random(19 , 121) * 121 );
-    console.log(Math.floor(Math.random(19 , 121) * 121 ))
+    console.log(randomScore)
 
 
 //functions to run game
@@ -15,31 +15,33 @@ var randomScore = Math.floor(Math.random(19 , 121) * 121 );
    $("#button2").click(clicked)
    $("#button3").click(clicked)
    $("#button4").click(clicked)
+   $("#randomScore").html("Score to match: " + randomScore)
+   $("#wins").html("wins" + wins)
+   $("#losses").html("losses" + losses)
+   
+   
 
 function clicked() {
     //push buttton value to player score array
     console.log('playersScore ' + $(this).val());
         playerScore = Number(playerScore) + Number($(this).val()) ;
+        $("#playerScore").html("Current Score: " + playerScore)
         console.log('Current Score: ' + playerScore);
-        if (playerScore === randomScore && playerScore !== 0) {
-
-            wins++ ;
-        
-        } else if (playerScore > randomScore) {
-        
-            losses++ ;
-        
-        }
-
-    }
+   
 
 //functions to count wins/losses
 
+if (playerScore == randomScore) {          
+    wins++ ;
+    
+
+} else if (playerScore > randomScore) {
+
+    losses++ ;
+
+} }
 
 
-
-
-// winLoss() ;
 console.log("wins" , wins)
 console.log("losses" , losses);
 })
