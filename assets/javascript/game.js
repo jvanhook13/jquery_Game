0b23077eb4e1684cd850f88a0f0c844f432b8b06
jvanhook13 <jaredvanhook1313@gmail.com>
2020-02-17 23:37:02 -0800

    updates css added images for buttons now shows game over or win message but does not increment wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,71 @@
-$(document).ready(function(){
-  
-  
+var wins = 0;
+var losses = 0;
+var gameWin = false
 
-//declare variables/objects
-var playerScore = 0 ;
-var wins = 0 ;
-var losses = 0 ;
-var randomScore = Math.floor(Math.random(19 , 121) * 121 );
+$(document).ready(function game() {
+
+
+
+    //declare variables/objects
+    var playerScore = 0;
+
+    var randomScore = Math.floor(Math.random(19, 121) * 121);
     console.log(randomScore)
 
 
-//functions to run game
-   $("#button1").click(clicked)
-   $("#button2").click(clicked)
-   $("#button3").click(clicked)
-   $("#button4").click(clicked)
-   $("#randomScore").html("Score to match: " + randomScore)
-   $("#wins").html("wins" + wins)
-   $("#losses").html("losses" + losses)
-   
-   
+    //functions to run game
+    $("#button1").click(clicked)
+    $("#button2").click(clicked)
+    $("#button3").click(clicked)
+    $("#button4").click(clicked)
+    $("#randomScore").html("Score to match: " + randomScore)
+    $("#wins").html("wins" + wins)
+    $("#losses").html("losses" + losses)
 
-function clicked() {
-    //push buttton value to player score array
-    console.log('playersScore ' + $(this).val());
-        playerScore = Number(playerScore) + Number($(this).val()) ;
+
+
+    function clicked() {
+        //push buttton value to player score array
+        console.log('playersScore ' + $(this).val());
+        playerScore = Number(playerScore) + Number($(this).val());
         $("#playerScore").html("Current Score: " + playerScore)
         console.log('Current Score: ' + playerScore);
-   
+        winLoss();
+        restart();
+    }
 
-//functions to count wins/losses
-
-if (playerScore == randomScore) {          
-    wins++ ;
-    
-
-} else if (playerScore > randomScore) {
-
-    losses++ ;
-
-} }
+    //functions to count wins/losses
+    function winLoss() {
+        if (playerScore === randomScore) {
+            gameWin = true
+            wins += 1;
+            $("#info").html("Winner Winner Chicken Dinner")
+            console.log("Winner Winner Chicken Dinner")
 
 
-console.log("wins" , wins)
-console.log("losses" , losses);
+        } else if (playerScore > randomScore) {
+            losses += 1;
+            $("#info").html("Better Luck next time")
+            console.log("Better Luck next time")
+
+        }
+    };
+
+
+
+    function restart() {
+        if (gameWin == true) {
+            game;
+
+        } else if (playerScore > randomScore) {
+
+            game;
+
+
+        }
+
+    }
+
+    console.log("wins", wins)
+    console.log("losses", losses);
 })
